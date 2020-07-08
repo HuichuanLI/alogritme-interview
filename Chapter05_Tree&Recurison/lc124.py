468ded110e145458193307dce531c4d47c52ee81
huichuan <lhc14124908@163.com>
2020-07-08 23:44:14 +0800

    add

---
+++ Chapter05_Tree&Recurison/lc124.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or p.val == root.val or q.val == root.val:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        if left:
+            return left
+
+        if right:
+            return right
+
+        return None
--- Chapter05_Tree&Recurison/leetcode110.py
+++ Chapter05_Tree&Recurison/leetcode110.py
@@ -3,7 +3,7 @@ class Solution:
         if not root:
             return True
         res = []
-        self.calculate_height(root,res)
+        self.calculate_height(root, res)
         if max(res) > 1:
             return False
         else:
@@ -16,3 +16,26 @@ class Solution:
         right_height = self.calculate_height(s.right)
         res.append(abs(left_height - right_height))
         return 1 + max(left_height, right_height)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.maxDepth(root) != -1
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
