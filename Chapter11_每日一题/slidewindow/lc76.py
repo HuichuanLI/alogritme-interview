f42efb1e66edd8c917213e735ff02f72ac944bc3
huichuan <lhc14124908@163.com>
2020-08-13 16:03:04 +0800

    add

--- Chapter11_每日一题/slidewindow/lc76.py
+++ Chapter11_每日一题/slidewindow/lc76.py
@@ -1,27 +1,30 @@
 from collections import Counter
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        need=collections.defaultdict(int)
-        for c in t:
-            need[c]+=1
-        needCnt=len(t)
-        i=0
-        res=(0,float('inf'))
-        for j,c in enumerate(s):
-            if need[c]>0:
-                needCnt-=1
-            need[c]-=1
-            if needCnt==0:       #步骤一：滑动窗口包含了所有T元素
-                while True:      #步骤二：增加i，排除多余元素
-                    c=s[i]
-                    if need[c]==0:
-                        break
-                    need[c]+=1
-                    i+=1
-                if j-i<res[1]-res[0]:   #记录结果
-                    res=(i,j)
-                need[s[i]]+=1  #步骤三：i增加一个位置，寻找新的满足条件滑动窗口
-                needCnt+=1
-                i+=1
-        return '' if res[1]>len(s) else s[res[0]:res[1]+1]
 
+
+class Solution:
+
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) == 0 or len(t) == 0:
+            return ""
+        res = ""
+        ans = sys.maxsize
+        j = 0
+        target_hash = Counter(t)
+        need = collections.defaultdict(int)
+        for c in t:
+            need[c] += 1
+        needCnt = len(t)
+        for i in range(len(s)):
+            while j < len(s) and not needCnt == 0:
+                if need[s[j]] > 0:
+                    needCnt -= 1
+                need[s[j]] -= 1
+                j += 1
+            if needCnt == 0:
+                if ans > j - i + 1:
+                    ans = j - i + 1
+                    res = s[i:j]
+                need[s[i]] += 1
+                if need[s[i]] > 0:
+                    needCnt += 1
+        return res
