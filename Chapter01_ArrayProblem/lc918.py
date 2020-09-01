ec30de5006382b6d028692bf158dabcebdcd667f
huichuan <lhc14124908@163.com>
2020-09-01 22:09:25 +0800

    add

--- Chapter01_ArrayProblem/lc918.py
+++ Chapter01_ArrayProblem/lc918.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Solution:
     def maxSubarraySumCircular(self, A: List[int]) -> int:
         n = len(A)
@@ -15,3 +18,20 @@ class Solution:
             return max(max_array)
         else:
             return max(max(max_array), sum(A) - min(min_array))
+
+
+class Solution:
+    def maxSubarraySumCircular(self, A: List[int]) -> int:
+        n = len(A)
+        result = -sys.maxsize
+        A = A + A
+
+        for i in range(2 * n):
+            res = [0]
+            res[0] = A[i]
+            for j in range(i + 1, min(i + n + 1, n)):
+                res.append(max(res[j - i - 1] + A[j], A[j]))
+            if max(res) > result:
+                result = max(res)
+
+        return result
