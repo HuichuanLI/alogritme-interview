4b8e098c18182410d7886c43b378f57841cd8025
huichuan <lhc14124908@163.com>
2020-09-02 14:43:33 +0800

    add

--- Chapter01_ArrayProblem/lc532.py
+++ Chapter01_ArrayProblem/lc532.py
@@ -4,10 +4,11 @@ class Solution:
         if n == 0:
             return False
         res = [0] * (n + 1)
-        result = set()
         for i in range(n):
             res[i + 1] = res[i] + nums[i]
-            if (res[i + 1] - k) in result:
-                return True
-            result.add(res[i + 1])
+            for j in range(i):
+                if k != 0 and (res[i + 1] - res[j]) % k == 0:
+                    return True
+                if k == 0 and res[i + 1] - res[j] == 0:
+                    return True
         return False
